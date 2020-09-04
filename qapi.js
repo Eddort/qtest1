a5811618aeef4a03a3c0ff4750af88a78ab72f33
Alexander <webisforce@gmail.com>
2020-09-04 18:25:13 +0300

    Some changes

---
+++ qapi.js
@@ -0,0 +1,24 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f525bec8580ae001e48e472",
+				"5f525bec8580ae001e48e474"
+			]
+		},
+		"5f525bec8580ae001e48e472": {
+			"id": "5f525bec8580ae001e48e472",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f525bec8580ae001e48e474": {
+			"id": "5f525bec8580ae001e48e474",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Text, Box } from "@quarkly/widgets";
+import { Section } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://images.unsplash.com/photo-1515600051222-a3c338ff16f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80) center/cover">
+			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
+				<Text
+					as="h4"
+					font="--base"
+					color="--grey"
+					letter-spacing="1px"
+					text-transform="uppercase"
+					margin="6px 0"
+				>
+					About me
+				</Text>
+				<Text as="h2" font="--headline2" margin="0 0 12px 0">
+					Hey I’m David
+				</Text>
+				<Text font="--base">
+					hjgjgjh
+				</Text>
+			</Box>
+		</Section>
+	</Theme>;
+});
